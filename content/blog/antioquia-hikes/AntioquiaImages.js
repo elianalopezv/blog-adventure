8a4c70fb9f96e8c5e54a1c793b354e402a816318
Eliana López <3lianalopez@gmail.com>
2020-02-08 19:33:06 -0500

    Mdx plugin added and initial mdx component

---
+++ content/blog/antioquia-hikes/AntioquiaImages.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import Stories from 'react-insta-stories';
+
+import avatar from '../../assets/profile-pic.png';
+import carolina from '../../assets/antioquia/carolina.jpg';
+import guatape from '../../assets/antioquia/guatape.jpeg';
+import laCeja from '../../assets/antioquia/la-ceja.jpeg';
+import venecia from '../../assets/antioquia/venecia.jpeg';
+import belmira from '../../assets/antioquia/belmira.jpeg';
+
+const stories = [
+    {
+      url: carolina,
+      header: {
+        heading: 'Carolina del Príncipe',
+        subheading: 'Lake, sun and forests.',
+        profileImage: avatar,
+      }
+    },
+    {
+      url: guatape,
+      header: {
+        heading: 'Guatapé -  San Rafael',
+        subheading: 'Big rock from the top.',
+        profileImage: avatar
+      }
+    },
+    {
+      url: laCeja,
+      header: {
+        heading: 'La Ceja',
+        subheading: 'Magic waterfall.',
+        profileImage: avatar
+      }
+    },
+    {
+      url: venecia,
+      header: {
+        heading: 'Venecia',
+        subheading: 'Most high natural pyramid in the world.',
+        profileImage: avatar
+      }
+    },
+    {
+      url: belmira,
+      header: {
+        heading: 'Belmira',
+        subheading: 'Unique ecosystem from Colombia.',
+        profileImage: avatar
+      }
+    },
+];
+
+
+class AntioquiaImages extends Component {
+	render() {
+		return (
+			<Stories
+				stories={stories}
+				defaultInterval={1000 * 5}
+				width={432}
+				height={768}
+			/>
+		);
+	}
+}
+
+export default AntioquiaImages;
